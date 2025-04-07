17946009f4b65fef76662419a0730980a41825bf
Liam Maricato <liammaricato@gmail.com>
2025-04-06 22:40:00 -0300

    Fix hydration problem

--- app/components/MeninasCarousel.tsx
+++ app/components/MeninasCarousel.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import {
   Carousel,
   CarouselContent,
@@ -9,26 +10,29 @@ import { useMediaQuery } from "react-responsive";
 import Autoplay from "embla-carousel-autoplay";
 
 export default function MeninasCarousel() {
+  const [isClient, setIsClient] = useState(false);
+
+  // Essa flag evita o problema de descompasso na renderização
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   const isMediumOrLarger = useMediaQuery({ query: "(min-width: 768px)" });
 
-  if (isMediumOrLarger) {
+  if (!isClient || isMediumOrLarger) { 
     return (
       <div className="meninas h-[60vh] flex flex-wrap w-full">
         <div className="menina kalo w-1/2 md:w-auto md:flex-auto bg-yellow"></div>
         <div className="menina nath w-1/2 md:w-auto md:flex-auto bg-pink"></div>
         <div className="menina mayu w-1/2 md:w-auto md:flex-auto bg-blue"></div>
         <div className="menina izu w-1/2 md:w-auto md:flex-auto bg-yellow"></div>
-        <div className="menina panshee w-1/2 md:w-auto md:flex-auto bg-pink"></div>
+        <div className="menina panshee w-full md:w-auto md:flex-auto bg-pink"></div>
       </div>
     );
   }
 
   return (
-    <Carousel
-      plugins={[
-        Autoplay({ delay: 4000 }),
-      ]}
-    >
+    <Carousel plugins={[Autoplay({ delay: 3500 })]}>
       <CarouselContent className="h-[60vh]">
         <CarouselItem>
           <div className="menina kalo h-full bg-yellow"></div>
