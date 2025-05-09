1c9e2b00eab8a5c3b2881b0441cf7a1793de65f5
Liam Maricato <liammaricato@gmail.com>
2025-05-09 17:17:04 -0300

    Fix import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import Talentos from "./components/Talentos";
-import Meninas from "./components/MeninasCarousel";
 
 export const runtime = "edge";
 
@@ -8,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <img src="/banner.png" alt="Banner Doki Live" className="w-full" />
-      
+
       <div className="flex flex-col md:flex-row w-full justify-between align-center bg-white overflow-hidden px-8 sm:px-[5%] lg:px-[10%] pt-14 pb-20 sm:py-32 gap-20">
         <div className="flex-1 flex flex-col gap-10 items-start">
           <h1 className="font-black text-4xl">Sobre</h1>
